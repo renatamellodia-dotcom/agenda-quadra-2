a306c6b8a91bda1636caa21f1ff25ed59c8d6b4c
renatamellodia-dotcom <renatamellodia@gmail.com>
2026-06-06 09:48:02 -0300

    Update main.jsx

--- melodia-project/src/main.jsx
+++ melodia-project/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cliente from './Cliente.jsx'
 import Admin from './Admin.jsx'
 import Funcionario from './Funcionario.jsx'
+import Confirmado from './Confirmado.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Cliente />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/funcionario" element={<Funcionario />} />
+      <Route path="/confirmado" element={<Confirmado />} />
     </Routes>
   </BrowserRouter>
 )
